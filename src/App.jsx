481ed449938d65bd58997c76d6cd4b8b88b31780
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-12-25 16:01:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Components/Counter';
+
+function App() {
+  return (
+    <>
+      <h1>Counter Example</h1>
+      {/* <Counter /> */}
+    </>
+  );
+}
+
+export default App;
